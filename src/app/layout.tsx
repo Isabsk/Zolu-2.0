eaee235e23da2ccc427b2e3de24f96dc8e8148a4
Isabsk <isabsk10@gmail.com>
2025-09-24 09:32:57 +0530

    Initial project commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/vercel.png" type="image/png" />
+        <Script
+        strategy="afterInteractive"
+        src="https://cdn-in.pagesense.io/js/zolu/539e35859de2428a917cc836e71889d2.js"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
