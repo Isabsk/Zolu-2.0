501de565c7d5b9dc9080cc86cf7a3ad0c2fcd013
Isabsk <isabsk10@gmail.com>
2025-09-29 14:23:47 +0530

    Initial project commit

--- src/app/colour/[color]/page.tsx
+++ src/app/colour/[color]/page.tsx
@@ -624,7 +624,7 @@ export default function ColorPage({ params }: { params: { color: string } }) {
 							rel="noopener noreferrer"
 							className="text-green-600 hover:underline"
 							>
-							Lacoste "SAVE OUR SPECIES" campaign
+							Lacoste &quot;SAVE OUR SPECIES&quot; campaign
 							</a>
 						</strong>
 					</ul>
